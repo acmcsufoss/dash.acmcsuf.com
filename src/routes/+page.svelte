80daf4f5a5f0d1a01f558c21eed803f490b48304
Josh Holman <git@jolman.me>
2026-01-18 21:55:13 -0800

    update content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-col items-center my-20">
+	<h1 class="text-5xl">Welcome to dash.acmcsuf.com</h1>
+</div>
